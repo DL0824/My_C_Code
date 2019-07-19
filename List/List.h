46d3b55e0260da3d23dd73942149b99a3c935653
DL0824 <18328607751@163.com>
2019-07-19 21:11:32 +0800

    双向循环链表

---
+++ List/List.h
@@ -0,0 +1,32 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+#include<stdio.h>
+#include<Windows.h>
+typedef int LTDataType;
+typedef struct ListNode
+{
+	LTDataType _data;
+	struct ListNode* _next;
+	struct ListNode* _prev;
+}ListNode;
+
+typedef struct List
+{
+	ListNode* _head;
+}List;
+
+void ListInit(List* plist);
+void ListDestory(List* plist);
+void ListPushBack(List* plist, LTDataType x);
+void ListPopBack(List* plist);
+void ListPushFront(List* plist, LTDataType x);
+void ListPopFront(List* plist);
+ListNode* ListFind(List* plist, LTDataType x);
+void ListInsertAfter(ListNode* pos, LTDataType x);
+void ListInsertFront(ListNode* pos, LTDataType x);
+void ListErase(ListNode* pos);
+void ListRemove(List* plist, LTDataType x);
+void ListPrint(List* plist);
+
+#endif/*_LIST_H_*/
